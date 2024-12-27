aa6dbe6945797026b1e1e43680a08ac005c33193
Guy Chait <53366531+gchait@users.noreply.github.com>
2024-12-27 18:40:37 +0200

    indent

--- Other/LinuxAdventures/TumbleweedPlasma/bootstrap.sh
+++ Other/LinuxAdventures/TumbleweedPlasma/bootstrap.sh
@@ -1,12 +1,12 @@
 set -euxo pipefail
 
 get_executable() {
-    wget -qO "$1" "$2"
-    chmod +x "$1"
+  wget -qO "$1" "$2"
+  chmod +x "$1"
 }
 
 get_repo() {
-    cd "$1" && git pull || git clone "$2" "$1"
+  cd "$1" && git pull || git clone "$2" "$1"
 }
 
 cp -r ~/gchait/Linux/Home/.* ~/
@@ -19,12 +19,12 @@ zypper addrepo https://yum.corretto.aws/corretto.repo 2> /dev/null || true
 zypper ref
 
 zypper in -y \
-    docker docker-buildx docker-compose vim netcat-openbsd \
-    zsh neofetch eza fastfetch figlet yq just ripgrep asciinema \
-    java-21-amazon-corretto-devel wget bind-utils gron helm jq
+  docker docker-buildx docker-compose vim netcat-openbsd \
+  zsh neofetch eza fastfetch figlet yq just ripgrep asciinema \
+  java-21-amazon-corretto-devel wget bind-utils gron helm jq
 
 get_executable /usr/local/bin/pfetch \
-    "https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch"
+  "https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch"
 
 get_repo ~/.zsh/pure https://github.com/sindresorhus/pure.git
 get_repo ~/.zsh/zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting
--- Other/LinuxAdventures/TumbleweedPlasma/graphical.sh
+++ Other/LinuxAdventures/TumbleweedPlasma/graphical.sh
@@ -8,20 +8,20 @@ opi codecs
 # flatpak install com.github.tchx84.Flatseal com.valvesoftware.Steam
 
 code_exts() {
-    code --install-extension catppuccin.catppuccin-vsc-icons
-    code --install-extension eamodio.gitlens
-    code --install-extension ecmel.vscode-html-css
-    code --install-extension esbenp.prettier-vscode
-    code --install-extension hashicorp.terraform
-    code --install-extension kokakiwi.vscode-just
-    code --install-extension ms-azuretools.vscode-docker
-    code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-    code --install-extension ms-pyright.pyright
-    code --install-extension pkief.material-product-icons
-    code --install-extension redhat.vscode-yaml
-    code --install-extension samuelcolvin.jinjahtml
-    code --install-extension tamasfe.even-better-toml
-    code --install-extension tinkertrain.theme-panda
+  code --install-extension catppuccin.catppuccin-vsc-icons
+  code --install-extension eamodio.gitlens
+  code --install-extension ecmel.vscode-html-css
+  code --install-extension esbenp.prettier-vscode
+  code --install-extension hashicorp.terraform
+  code --install-extension kokakiwi.vscode-just
+  code --install-extension ms-azuretools.vscode-docker
+  code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+  code --install-extension ms-pyright.pyright
+  code --install-extension pkief.material-product-icons
+  code --install-extension redhat.vscode-yaml
+  code --install-extension samuelcolvin.jinjahtml
+  code --install-extension tamasfe.even-better-toml
+  code --install-extension tinkertrain.theme-panda
 }
 
 code_exts > /dev/null
