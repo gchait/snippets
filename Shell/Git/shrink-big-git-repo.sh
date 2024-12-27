21b6894818ed2667b6b8bf23401ca774e5849110
Guy Chait <53366531+gchait@users.noreply.github.com>
2024-12-27 13:29:17 +0200

    just in case

--- Shell/Git/shrink-big-git-repo.sh
+++ Shell/Git/shrink-big-git-repo.sh
@@ -1,5 +1,5 @@
 sudo wget -qO /opt/bfg.jar https://repo1.maven.org/maven2/com/madgag/bfg/1.14.0/bfg-1.14.0.jar
-java -jar /opt/bfg.jar -b 1M
+java -jar /opt/bfg.jar -b 2M
 
 git reflog expire --expire=now --all
 git gc --prune=now --aggressive
