2afe720893b2549d297b6aeae4dccdd73442e594
Guy Chait <53366531+gchait@users.noreply.github.com>
2026-03-16 22:13:30 +0200

    fmt

--- Shell/Git/promote-monochart.sh
+++ Shell/Git/promote-monochart.sh
@@ -1,19 +1,22 @@
-# shellcheck disable=SC2154,SC2016
+# shellcheck disable=SC2016,SC2154,SC2155,SC2164
 
 # If full merge
-# shellcheck disable=SC2164
 cd "${manifest_dir}"
 git checkout "${dest_branch}"
-git merge "${src_branch}" --no-commit --strategy-option theirs
-rm -f .git/MERGE_HEAD
+git merge -s ours --no-commit "${src_branch}"
+git read-tree -u --reset "${src_branch}"
+git commit -m "Merge ${src_branch} into ${dest_branch}" || true
+git push origin "${dest_branch}"
+git checkout - || true
 
 # If single service
 cd "${manifest_dir}"
-# shellcheck disable=SC2155
 export new_tag=$(yq '.${specific_app}.bbbbb' xxxx/yyyy.yaml)
 git checkout "${dest_branch}"
 yq '. | .${specific_app}.bbbbb = env(new_tag)' xxxx/yyyy.yaml > yyyy.yaml.tmp &&
   mv yyyy.yaml.tmp xxxx/yyyy.yaml
+git add xxxx/yyyy.yaml
 git checkout "${src_branch}" -- "zxxxx/${specific_app}.yaml"
-
-### PUSH LOGIC GOES HERE ###
+git commit -m "Promote ${specific_app} from ${src_branch} to ${dest_branch}" || true
+git push origin "${dest_branch}"
+git checkout - || true
